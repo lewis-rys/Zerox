5dd2b36537892c778eaeb5ca577811b4e7704828
Ariel Francisco <arielfranciscomartinezreyes@gmail.com>
2025-10-09 16:56:28 -0600

    guardado actual

--- funcionality.js
+++ funcionality.js
@@ -7,9 +7,9 @@ if (hamburguesa && enlaces) {
   });
 }
 
-// Funciones del modal
-function abrirModal() {
-  const template = document.getElementById('modalGeneralTemplate');
+// Funciones del modal Contacto
+function abrirModalcontacto() {
+  const template = document.getElementById('modalGeneralcontacto');
   const clone = template.content.cloneNode(true);
   const modalContent = document.getElementById('modalContent');
   modalContent.innerHTML = ''; // Limpiar contenido anterior
@@ -19,6 +19,42 @@ function abrirModal() {
   modal.style.display = 'flex';
 }
 
+// Funciones del modal Video
+function abrirModalvideo() {
+  const template = document.getElementById('modalGeneralvideo');
+  const clone = template.content.cloneNode(true);
+  const modalContent = document.getElementById('modalContent');
+  modalContent.innerHTML = ''; // Limpiar contenido anterior
+  modalContent.appendChild(clone);
+  
+  const modal = document.getElementById('modalGeneralContainer');
+  modal.style.display = 'flex';
+}
+
+// Funciones del modal Drogas
+function abrirModaldrogas() {
+  const template = document.getElementById('modalGeneraldrogas');
+  const clone = template.content.cloneNode(true);
+  const modalContent = document.getElementById('modalContent');
+  modalContent.innerHTML = ''; // Limpiar contenido anterior
+  modalContent.appendChild(clone);
+  
+  const modal = document.getElementById('modalGeneralContainer');
+  modal.style.display = 'flex';
+}
+
+// Funciones del modal Inicio
+function abrirModalinicio() {
+  const template = document.getElementById('modalGeneralinicio');
+  const clone = template.content.cloneNode(true);
+  const modalContent = document.getElementById('modalContent');
+  modalContent.innerHTML = ''; // Limpiar contenido anterior
+  modalContent.appendChild(clone);
+  
+  const modal = document.getElementById('modalGeneralContainer');
+  modal.style.display = 'flex';
+}
+// funcion general cerrar modal
 function cerrarModalGeneral() {
   const modal = document.getElementById('modalGeneralContainer');
   modal.style.display = 'none';
